337f166384e26086be5b64285d01e1db81c7d19a
Rafael JPD <rafael.pezzuto@gmail.com>
2021-11-09 22:06:33 -0300

    adiciona setup.py e requirements.txt

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+install_requirements=[
+    'mysqlclient',
+    'requests',
+    'reverse_geocoder',
+    'sqlalchemy',
+    'scielo_log_validator',
+]
+
+setup(
+    name='scielo-usage-counter',
+    version='0.1',
+    description='The SciELO Usage Counter Tool',
+    author='SciELO',
+    author_email='scielo-dev@googlegroups.com',
+    license='BSD',
+    install_requires=install_requirements,
+    url='https://github.com/scieloorg/scielo_usage_counter',
+    keywords='usage access, project counter r5, sushi',
+    maintainer_email='rafael.pezzuto@gmail.com',
+    packages=find_packages(),
+)

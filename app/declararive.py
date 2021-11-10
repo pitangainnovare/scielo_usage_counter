838c879d3ca8556ad8aa9eadc00f10014a40dd45
Rafael JPD <rafael.pezzuto@gmail.com>
2021-11-09 22:13:27 -0300

    adiciona tabela collection

--- app/declararive.py
+++ app/declararive.py
@@ -7,3 +7,13 @@ from sqlalchemy.orm import declarative_mixin
 
 Base = declarative_base()
 
+
+
+class Collection(Base):
+    __tablename__ = 'collection'
+    __table_args__ = (UniqueConstraint('acronym',),)
+
+    id = Column(TINYINT(unsigned=True), primary_key=True, autoincrement=True)
+
+    acronym = Column(VARCHAR(3), nullable=False)
+    name = Column(VARCHAR(128), nullable=False, index=True)

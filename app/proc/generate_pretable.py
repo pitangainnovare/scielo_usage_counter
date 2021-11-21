2394ce27ad87a854486a740002da7ca14320d96f
Rafael JPD <rafael.pezzuto@gmail.com>
2021-11-20 21:49:08 -0300

    adiciona método para gravar ou atualizar pré-tabela

--- app/proc/generate_pretable.py
+++ app/proc/generate_pretable.py
@@ -89,3 +89,56 @@ def read_pretable(filepath, delimiter='\t', ignore_header=True):
         if ignore_header:
             fin.readline()
         return [d.strip().split(delimiter) for d in fin]
+
+
+def write_pretable(filepath, header, sort_field, data, delimiter='\t'):
+    """
+    Grava arquivo de pré-tabela.
+    Caso arquivo já exista, lê dados pré-existentes e gera uma versão ordenada dos dados.
+
+    Parameters
+    ----------
+    filepath : str
+        Nome do arquivo de pré-tabela
+    header: list
+        Lista de strings que representam o cabeçalho do arquivo de saída
+    sort_field: str
+        Nome do campo de ordenação
+    data: dict
+        Dicionário contendo valores de acesso
+    delimiter: str
+        Separador de colunas do arquivo de pré-tabela
+
+    Returns
+    -------
+    str
+        Caminho do arquivo de pré-tabela gravado
+    """
+    tmp_data = [[i.get(h) for h in header] for i in data]
+
+    if os.path.exists(filepath):
+        # cria cópia de backup de arquivo pré-existente
+        bak_output = create_backup(filepath)
+        logging.info('Gerado backup %s' % bak_output)
+
+        # lê dados dados pré-existentes
+        tmp_data += read_pretable(filepath, delimiter)
+
+    # cria arquivo com cabeçalho conteúdo
+    create_file_with_header(filepath, header)
+
+    # remove linhas repetidas
+    lines = set([delimiter.join(i) for i in tmp_data])
+
+    # ordena as linhas de acordo com o IP
+
+    logging.info('Ordenando dados')
+    sorted_lines = sorted([l.split(delimiter) for l in lines], key=lambda x: ipaddress.IPv4Address(x[header.index(sort_field)]))
+
+    logging.info('Gravando dados em %s' % filepath)
+    with open(filepath, 'w') as fout:
+        fout.write(delimiter.join(header) + '\n')
+        for i in sorted_lines:
+            fout.write(delimiter.join(i) + '\n')
+
+    return filepath

d7fd5a75736012dbed0ca6397216888f2b3d92e0
Rafael J P D <rafael.pezzuto@gmail.com>
2021-12-08 18:55:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requirements=[
 
 setup(
     name='scielo-usage-counter',
-    version='0.3.1',
+    version='0.3.3',
     description='The SciELO Usage Counter Tool',
     author='SciELO',
     author_email='scielo-dev@googlegroups.com',

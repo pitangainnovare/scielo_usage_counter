5f166b47c07fe4e090005703c0f7c58b9edceb4a
Rafael JPD <rafael.pezzuto@gmail.com>
2022-01-10 19:40:32 -0300

    remove espaços inválidos do código-fonte

--- app/lib/db.py
+++ app/lib/db.py
@@ -64,7 +64,7 @@ def _get_previous_and_next_dates(date, interval=2):
 
 def _get_enabled_dates_by_status_value(session, collection, date_status: dict, status_value: int):
     enabled_dates = []
-        
+
     for date, status in date_status.items():
         pn_dates = _get_previous_and_next_dates(date)
         is_valid_date = _check_previous_and_next_dates(session, collection, pn_dates)
@@ -103,9 +103,9 @@ def get_non_pretable_dates(str_connection, collection):
                 models.ControlDateStatus.status == values.DATE_STATUS_LOADED,
             )
         ).order_by(models.ControlDateStatus.date.desc())
-        
+
         date2status = _get_date_status(parsed_dates)
-        
+
         return _get_enabled_dates_by_status_value(session, collection, date2status, values.DATE_STATUS_LOADED)
 
     except NoResultFound:
@@ -143,7 +143,7 @@ def get_logfile_status(str_connection, logfile_id):
 
 def set_logfile_status(str_connection, logfile_id, status):
     session = get_session(str_connection)
-    lf = session.get(models.ControlLogFile, logfile_id) 
+    lf = session.get(models.ControlLogFile, logfile_id)
     lf.status = status
     session.commit()
 
--- app/proc/parse_log.py
+++ app/proc/parse_log.py
@@ -51,14 +51,14 @@ def parse_file(logfile: str, output_directory: str, mmdb: str, robots: str):
         data = lp.parse()
         lp.save(data)
 
-        logging.info(f'Arquivo {logfile} foi processado em {lp.total_time} segundos')        
+        logging.info(f'Arquivo {logfile} foi processado em {lp.total_time} segundos')
         return values.LOGFILE_STATUS_LOADED
     else:
         logging.warning(f'Arquivo {logfile} foi invalidado')
         return values.LOGFILE_STATUS_INVALIDATED
 
 
-def parse_files_db(str_connection: str, collection: str, output_directory: str, mmdb: str, robots: str):    
+def parse_files_db(str_connection: str, collection: str, output_directory: str, mmdb: str, robots: str):
     non_parsed_logs = db.get_non_parsed_logs(str_connection, collection)
 
     for lf in non_parsed_logs:

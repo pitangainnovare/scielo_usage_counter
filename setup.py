b1c179bca0d2f90763c3d51aa7588a86f3e17e7c
Rafael JPD <rafael.pezzuto@gmail.com>
2022-04-11 13:51:24 -0300

    Atualiza setup.py para 0.4.6.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requirements=[
 
 setup(
     name='scielo-usage-counter',
-    version='0.4.6',
+    version='0.4.6.1',
     description='The SciELO Usage Counter Tool',
     author='SciELO',
     author_email='scielo-dev@googlegroups.com',

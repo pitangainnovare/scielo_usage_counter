f88bb6ec5514e5bdecd5b3f1d7c9c44a2c12629f
Rafael JPD <rafael.pezzuto@gmail.com>
2025-03-24 11:13:59 -0300

    Adequa script bash para parsing de logs

--- scielo_usage_counter/proc/parse_log.py
+++ scielo_usage_counter/proc/parse_log.py
@@ -6,13 +6,7 @@ import os
 from scielo_log_validator import validator
 from scielo_usage_counter import log_handler, values
 from scielo_usage_counter.utils import file_utils
-from scielo_usage_counter.database import db 
 
-
-STR_CONNECTION = os.environ.get(
-    'PARSE_LOG_STR_CONNECTION',
-    'mysql://user:pass@localhost:port/database',
-)
 
 COLLECTION = os.environ.get(
     'PARSE_LOG_COLLECTION',
@@ -24,25 +18,23 @@ LOGGING_LEVEL = os.environ.get(
     'INFO'
 )
 
-STR_CONNECTION = os.environ.get(
-    'PARSE_LOG_STR_CONNECTION',
-    'mysql://user:pass@localhost:3306/usage'
-)
-
 OUTPUT_DIRECTORY = os.environ.get(
     'OUTPUT_DIRECTORY',
     'data'
 )
 
 
-def parse_file(logfile: str, output_directory: str, mmdb: str, robots: str):
-    logging.info(f'Validação iniciada para arquivo {logfile}')
-    validation_results = validator.pipeline_validate(
-        path=logfile, 
-        sample_size=0.05
-    )
+def parse_file(logfile: str, output_directory: str, mmdb: str, robots: str, sample_size=0.05, validate=True):
+    validation_results = {}
 
-    if validation_results.get('is_valid', {}).get('all', False):
+    if validate:
+        logging.info(f'Validação iniciada para arquivo {logfile}')
+        validation_results = validator.pipeline_validate(
+            path=logfile, 
+            sample_size=sample_size
+        )
+
+    if validation_results.get('is_valid', {}).get('all', False) or not validate:
         output_filepath = file_utils.generate_filepath(output_directory, logfile)
 
         lp = log_handler.LogParser(mmdb_path=mmdb, robots_path=robots)
@@ -59,15 +51,6 @@ def parse_file(logfile: str, output_directory: str, mmdb: str, robots: str):
     else:
         logging.warning(f'Arquivo {logfile} foi invalidado')
         return values.LOGFILE_STATUS_INVALIDATED
-
-
-def parse_files_db(str_connection: str, collection: str, output_directory: str, mmdb: str, robots: str):
-    non_parsed_logs = db.get_non_parsed_logs(str_connection, collection)
-
-    for lf in non_parsed_logs:
-        lf_path = file_utils.translate_path(lf.full_path)
-        lf_status = parse_file(lf_path, output_directory, mmdb, robots)
-        db.set_logfile_status(str_connection, lf.id, lf_status)
 
 
 def main():
@@ -94,32 +77,10 @@ def main():
         help='Diretório de saída',
     )
 
-    subparsers = parser.add_subparsers(
-        title='mode',
-    )
-
-    file_parser = subparsers.add_parser('file', help='Modo de caminho de arquivo')
-
-    file_parser.add_argument(
+    parser.add_argument(
         '-f',
         '--logfile',
         help='Caminho de arquivo de log de acesso',
-    )
-
-    database_parser = subparsers.add_parser('database', help='Modo de banco de dados')
-
-    database_parser.add_argument(
-        '-u',
-        '--str_connection',
-        default=STR_CONNECTION,
-        help='String de conexão com banco de dados',
-    )
-
-    database_parser.add_argument(
-        '-c',
-        '--collection',
-        default=COLLECTION,
-        help='Acrônimo de coleção',
     )
 
     args = parser.parse_args()
@@ -130,9 +91,4 @@ def main():
         datefmt='%d/%b/%Y %H:%M:%S',
     )
 
-    if getattr(args, 'logfile', None):
-        logging.info('Inicializado em modo de arquivo')
-        parse_file(**args.__dict__)
-    elif getattr(args, 'str_connection', None):
-        logging.info('Inicializado em modo de banco de dados')
-        parse_files_db(**args.__dict__)
+    parse_file(**args.__dict__)

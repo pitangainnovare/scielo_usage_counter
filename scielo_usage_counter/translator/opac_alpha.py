ff3866224033d4dad1122468eb2964aa5413c82b
Rafael JPD <rafael.pezzuto@gmail.com>
2025-03-24 11:08:49 -0300

    Transfere tradutor de opac para módulo translator/*.py

---
+++ scielo_usage_counter/translator/opac_alpha.py
@@ -0,0 +1,123 @@
+import re
+
+from urllib.parse import urlparse, parse_qsl
+
+from scielo_usage_counter.values import (
+    MEDIA_FORMAT_HTML,
+    MEDIA_FORMAT_PDF,
+    MEDIA_LANGUAGE_UNDEFINED,
+)   
+
+
+# Patterns to support parameter extraction and determine whether a URL is an Investigation or a Request
+REGEX_OPAC_ALPHA_ARTICLE_ACRONYM_YEAR_VOL_ISSUE_LANGUAGE = re.compile(r'article/(?P<journal_acronym>\w*)/(?P<year>\d{0,4})\.(?P<vol_issue>[\d|\w]+)/(?P<pages>[\d|-]*)/(?P<language>\w{2})', re.IGNORECASE) # Request or Investigation
+REGEX_OPAC_ALPHA_PDF_ACRONYM_YEAR_VOL_ISSUE_LANGUAGE = re.compile(r'pdf/(?P<journal_acronym>\w*)/(?P<year>\d{0,4})\.(?P<vol_issue>[\d|\w]+)/(?P<pages>[\d|-]*)/(?P<language>\w{2})', re.IGNORECASE) # Request
+REGEX_OPAC_ALPHA_PDF_PATH = r'.*/?pdf/(?P<journal_acronym>\w*)/(.*)'    # Request
+REGEX_OPAC_ALPHA_MEDIA_ASSETS_ACRONYM = re.compile(r'.*/media/assets/(?P<journal_acronym>\w*)/?([\d|\w|\.|\-]*)/?([\d|\w|\-|\.|\_]*)', re.IGNORECASE)   # Request
+REGEX_OPAC_ALPHA_RESOURCE_SSM_PATH = r'.*resource_ssm_path=(?P<journal_acronym>\w*)/(?P<year_vol_issue>[\d|\w|\.|\-]*)/(?P<file>[\d|\w|\.|\-]*)'    # Request
+
+
+class URLTranslatorOPACAlphaSite:
+    def __init__(self, journals_metadata, articles_metadata):
+        self.journals_metadata = journals_metadata
+        self.articles_metadata = articles_metadata
+
+    def pipeline_translate(self, url):
+        self.url_params = self.extract_url_params(url)
+
+        pid_v3 = self.extract_pid_v3()
+        media_format = self.extract_media_format()
+        media_language = self.extract_media_language(pid_v3)
+        scielo_issn = self.extract_issn()
+
+        return {
+            'scielo_issn': scielo_issn,
+            'pid_v2': None,
+            'pid_v3': pid_v3,
+            'media_format': media_format,
+            'media_language': media_language,
+        }
+
+    def extract_url_params(self, url):
+        url_params = {
+            'pid_v3': None, 
+            'journal_acronym': None,
+            'media_format': None,
+            'media_language': None,
+            'resource_ssm_path': None,
+        }
+
+        url_parsed = urlparse(url)
+        if 'resource_ssm_path' in url:
+            self._get_url_params_from_query_key(url_params, url_parsed.query)
+        else:
+            self._get_url_params_from_url_path(url_params, url_parsed.path)
+
+        return url_params
+
+    def _get_url_params_from_query_key(self, url_params, query):
+        resource_path = dict(parse_qsl(query)).get('resource_ssm_path', '')
+        match = re.search(REGEX_OPAC_ALPHA_RESOURCE_SSM_PATH, resource_path)
+        if match:
+            url_params['journal_acronym'] = match.groupdict().get('journal_acronym')
+            url_params['year_vol_issue'] = match.groupdict().get('year_vol_issue')
+            url_params['file'] = match.group.groupdict().get('file')
+            url_params['resource_ssm_path'] = resource_path
+
+            if query.endswith('.pdf'):
+                url_params['media_format'] = MEDIA_FORMAT_PDF
+
+    def _get_url_params_from_url_path(self, url_params, url_path):
+        for media_format, p in [
+            (MEDIA_FORMAT_HTML, REGEX_OPAC_ALPHA_ARTICLE_ACRONYM_YEAR_VOL_ISSUE_LANGUAGE),
+            (MEDIA_FORMAT_PDF, REGEX_OPAC_ALPHA_PDF_ACRONYM_YEAR_VOL_ISSUE_LANGUAGE),
+            (MEDIA_FORMAT_PDF, REGEX_OPAC_ALPHA_PDF_PATH),
+        ]:
+    
+            match = re.search(p, url_path)
+
+            if match:
+                url_params['media_format'] = media_format
+                url_params['journal_acronym'] = match.groupdict().get('journal_acronym')
+                url_params['year'] = match.groupdict().get('year')
+                url_params['vol_issue'] = match.groupdict().get('vol_issue')
+                url_params['pages'] = match.groupdict().get('pages')
+                url_params['media_language'] = match.groupdict().get('language')
+                return
+
+        path_match_assets = re.match(REGEX_OPAC_ALPHA_MEDIA_ASSETS_ACRONYM, url_path)
+        if path_match_assets:
+            url_params['journal_acronym'] = path_match_assets.group(1)
+            url_params['year_vol_issue'] = path_match_assets.group(2)
+            url_params['file'] = path_match_assets.group(3)
+
+            if url_path.endswith('.pdf'):
+                url_params['media_format'] = MEDIA_FORMAT_PDF
+
+    def extract_media_format(self):
+        return self.url_params.get('media_format')
+
+    def extract_media_language(self, pid_v3):
+        media_language = self.url_params.get('media_language')
+        if not media_language:
+            media_language = self.articles_metadata['pid_v3_to_default_lang'].get(pid_v3, MEDIA_LANGUAGE_UNDEFINED)
+        return media_language
+
+    def extract_pid_v3(self):
+        return self._get_ssp_pid()
+    
+    def _get_ssp_pid(self):        
+        artificial_pid = ':'.join([
+            self.url_params.get('journal_acronym') or '',
+            self.url_params.get('year') or '',
+            self.url_params.get('vol_issue') or '',
+        ])
+
+        if self.url_params.get('pages'):
+            return ':'.join([artificial_pid, self.url_params.get('pages', '')])
+
+        if self.url_params.get('file'):
+            return ':'.join([artificial_pid, self.url_params.get('file', '')])
+    
+    def extract_issn(self):
+        return self.journals_metadata['acronym_to_scielo_issn'].get(self.url_params.get('journal_acronym'))

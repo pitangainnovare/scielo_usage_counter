3fc685758b29a5b7f5f4473324fd62c0d5c41f7b
Rafael JPD <rafael.pezzuto@gmail.com>
2025-03-24 11:00:02 -0300

    Atualiza setup.py para 1.1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requirements=[
 
 setup(
     name='scielo-usage-counter',
-    version='1.0.0',
+    version='1.1.0',
     description='The SciELO Usage Counter Tool',
     author='SciELO',
     author_email='scielo-dev@googlegroups.com',

eb1bba328576954b0868fa8de2dc59085641c4b7
Rafael JPD <rafael.pezzuto@gmail.com>
2025-03-26 14:27:18 -0300

    Remove lib mysql desnecessária e atualiza app para 1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 install_requirements=[
     'device-detector',
     'geoip2',
-    'mysqlclient',
     'requests',
     'reverse_geocoder',
     'scielo_log_validator',
@@ -12,7 +11,7 @@ install_requirements=[
 
 setup(
     name='scielo-usage-counter',
-    version='1.1.0',
+    version='1.2.0',
     description='The SciELO Usage Counter Tool',
     author='SciELO',
     author_email='scielo-dev@googlegroups.com',

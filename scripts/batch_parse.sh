b18f9e89dd3242b20768fa38602c0250c3925183
Rafael JPD <rafael@pitangainnovare.com.br>
2026-01-15 12:57:03 -0300

    Atualiza batch parse

--- scripts/batch_parse.sh
+++ scripts/batch_parse.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 help(){
 	echo "SciELO Usage COUNTER - Batch script Parse Log"
 	echo "Please, inform:"
@@ -6,47 +7,47 @@ help(){
 	echo "   2. The file robots (parameter -r)"
 	echo "   3. The output directory (parameter -o)"
 	echo "   4. The list of files paths (parameter -f)"
+	echo "   5. Disable validation (optional, parameter -n)"
 	echo ""
 	echo "For example:"
 	echo ""
-	echo "   scripts/batch_parse.sh -m data/map.mmdb -r data/counter-robots.txt -o data -f logs_paths.txt"
+	echo "   scripts/batch_parse.sh -m data/map.mmdb -r data/counter-robots.txt -o data -f logs_paths.txt -n"
 	echo ""
 }
 
 run(){
-	FILE_MMDB=$1;
-	FILE_ROBOTS=$2;
-	OUTPUT_DIR=$3;
-	INPUT=$4;
+	FILE_MMDB=$1
+	FILE_ROBOTS=$2
+	OUTPUT_DIR=$3
+	INPUT=$4
+	NO_VAL=$5
 
-	for i in `cat "$INPUT"`; do
-		LOGFILE=$i;
+	for i in $(cat "$INPUT"); do
+		LOGFILE=$i
+		echo "[Processando] $LOGFILE"
 
-		echo "[Processando] $LOGFILE";
-		parse-log -m "$FILE_MMDB" -r "$FILE_ROBOTS" -o "$OUTPUT_DIR" file -f $LOGFILE;
+		if [[ "$NO_VAL" == "1" ]]; then
+			parse-log -m "$FILE_MMDB" -r "$FILE_ROBOTS" -o "$OUTPUT_DIR" -f "$LOGFILE" --no-validate
+		else
+			parse-log -m "$FILE_MMDB" -r "$FILE_ROBOTS" -o "$OUTPUT_DIR" -f "$LOGFILE"
+		fi
 	done
 }
 
-while getopts f:m:r:o: opts; do
+NO_VAL=0
+while getopts f:m:r:o:n opts; do
 	case ${opts} in
-		# Diretório de arquivos de log ou arquivo indicando caminhos dos arquivos a serem processados
-      	f) INPUT=${OPTARG} ;;
-
-		# Arquivo de mapas em formato mmdb
+    f) INPUT=${OPTARG} ;;
 		m) FILE_MMDB=${OPTARG} ;;
-
-		# Arquivo de bots em formato txt
-	  	r) FILE_ROBOTS=${OPTARG} ;;
-
-		# Diretório de resultados
-		o) OUTPUT_DIR=${OPTARG} ;;
+    r) FILE_ROBOTS=${OPTARG} ;;
+    o) OUTPUT_DIR=${OPTARG} ;;
+    n) NO_VAL=1 ;;
 	esac
 done
 
-if [[ -z "$INPUT" || -z "$FILE_ROBOTS" || -z "$FILE_MMDB"  || -z "$OUTPUT_DIR" ]]
-	then
-		help;
-		exit;
-	else
-		run $FILE_MMDB $FILE_ROBOTS $OUTPUT_DIR $INPUT;
+if [[ -z "$INPUT" || -z "$FILE_ROBOTS" || -z "$FILE_MMDB"  || -z "$OUTPUT_DIR" ]]; then
+	help
+	exit
+else
+	run "$FILE_MMDB" "$FILE_ROBOTS" "$OUTPUT_DIR" "$INPUT" "$NO_VAL"
 fi

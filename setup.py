cc4fee846175e5cc700c094a53b75a01d532dd0d
Rafael JP Damaceno <rafael@pitangainnovare.com.br>
2026-08-30 10:31:12 -0300

    Atualiza a versão da biblioteca para 2.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requirements=[
 
 setup(
     name='scielo-usage-counter',
-    version='2.1.0',
+    version='2.2.0',
     description='The SciELO Usage Counter Tool',
     author='Rafael JP Damaceno',
     author_email='rafael@pitangainnovare.com.br',

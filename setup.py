428c34ad3e617fe9369b57529d46e4c680a73134
Rafael JP Damaceno <rafael@pitangainnovare.com.br>
2026-09-12 08:43:13 -0300

    Atualiza a versão para 0.6.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requirements=[
 
 setup(
     name='scielo-usage-counter',
-    version='0.6.3',
+    version='0.6.4',
     description='The SciELO Usage Counter Tool',
     author='SciELO',
     author_email='scielo-dev@googlegroups.com',
